093ecf43ffe11504c74d87a8da840d655c8b82ad
Nevin M K <nevin.madhukar.k@gmail.com>
2026-09-20 11:58:03 +0530

    Revert "Merge pull request #2 from nevinmk/feat/04-reviews-ssr-fragment"
    This reverts commit de92845122ea713f6e9962379afe59178abe6a86, reversing
    changes made to a89bad0e1275d2c23d347e97c8ec61100f076427.

--- 04-micro-frontends/remote-reviews/src/entry-server.tsx
+++
@@ -1,9 +0,0 @@
-import { renderToString } from "react-dom/server";
-import { Reviews } from "./Reviews";
-import { loadReviews } from "./loader";
-
-// The server half of this remote. server.mjs calls it per request.
-export async function render() {
-  const state = await loadReviews();
-  return { html: renderToString(<Reviews initial={state} />), state };
-}
--- scripts/constants/ports.mjs
+++ scripts/constants/ports.mjs
@@ -12,7 +12,6 @@ export const PORTS = {
   graphqlWeb: 3007,
   remoteProducts: 3041,
   remoteCart: 3042,
-  remoteReviews: 3043,
   graphqlApi: 3071,
 };
 
